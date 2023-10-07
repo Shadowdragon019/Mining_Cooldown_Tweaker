45a942a332c5af0cbc156ccfb77927575db2e78e
Shadowdragon019 <67480398+Shadowdragon019@users.noreply.github.com>
2023-10-07 13:36:53 -0500

    Initial commit

---
+++ src/main/java/mining_cooldown_tweaker/MiningCooldownTweaker.java
@@ -0,0 +1,38 @@
+package mining_cooldown_tweaker;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(MiningCooldownTweaker.mod_id)
+public class MiningCooldownTweaker {
+    public static final String mod_id = "mining_cooldown_tweaker";
+    private static final Logger logger = LogUtils.getLogger();
+
+    public MiningCooldownTweaker() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+
+    }
+
+    @Mod.EventBusSubscriber(modid = mod_id, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+
+        }
+    }
+}
